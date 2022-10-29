b8b9b644a4be1e358558a89c158d0eb5f98ce727
Gabriel Cavalcante <109037207+gabrielcavalcante-dev@users.noreply.github.com>
2022-10-29 00:04:02 -0300

    Add files via upload

---
+++ yt_download.py
@@ -0,0 +1,23 @@
+#importe a biblioteca
+from pytube import YouTube
+
+#link de entrada
+link = input("Cole o link para iniciar o download: ")
+yt = YouTube(link)
+
+#Informações sobre o vídeo
+result = {
+    "Titulo": yt.title,
+    "Tamanho do vídeo": yt.length,
+    "Número de views": yt.views,
+    "Avaliação de vídeo": yt.rating
+}
+print(result)
+
+#busca a melhor resolução para o vídeo
+ys = yt.streams.get_highest_resolution()
+print("Baixando...")
+
+#executa o download
+ys.download()
+print("Download concluído")
